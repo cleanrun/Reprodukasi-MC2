7be251bb98c7f5b41359fd23b99c15a9a549e5ea
aldialfian19 <105551380+aldialfian19@users.noreply.github.com>
2022-07-03 21:28:36 +0800

    hifi material

--- Reprodukasi/Controller/Materials/MaterialModel/MaterialData.swift
+++ Reprodukasi/Controller/Materials/MaterialModel/MaterialData.swift
@@ -16,23 +16,23 @@ class MaterialData{
         var arrMaterial2 = [MaterialModel]()
         var arrMaterial3 = [MaterialModel]()
         
-        let a = MaterialModel(title: "Masa tidak suka ikan?", detail: "Padahal ikan enak dan bermanfaat loh! Pelajari lebih lanjut!", image: "materiIkan")
-        arrMaterial.append(a)
     
-        let b = MaterialModel(title: "Hayo loh yang rokok", detail: "Masa sih rokok bisa merusak kesehatan reproduksi?", image: "materiRokok")
+        let a = MaterialModel(title: "Hayo loh yang rokok", detail: "Masa sih rokok bisa merusak kesehatan reproduksi?", image: "materiRokok")
+        arrMaterial.append(a)
+        
+        let b = MaterialModel(title: "Olahraga intensitas tinggi tidak baik", detail: "Ha? Masa sih olahraga berintensitas tinggi itu tidak baik?", image: "materiOlahraga")
         arrMaterial.append(b)
         
-        let c = MaterialModel(title: "Sunat tidak hanya untuk tradisi!", detail: "Sunat sebagai tradisi atau sunat untuk kesehatan? Mari pelajari bersama!", image: "")
+        let c = MaterialModel(title: "Sabun Kewanitaan Menyebabkan Iritasi?", detail: "Yang sering menggunakan sabun kewanitaan harus baca materi ini.", image: "materiSabun")
         arrMaterial2.append(c)
         
-        let d = MaterialModel(title: "Sabun Kewanitaan Menyebabkan Iritasi?", detail: "Yang sering menggunakan sabun kewanitaan harus baca materi ini.", image: "materiSabun")
-        arrMaterial2.append(d)
+        let d = MaterialModel(title: "Masa tidak suka ikan?", detail: "Padahal ikan enak dan bermanfaat loh! Pelajari lebih lanjut!", image: "materiIkan")
+        arrMaterial3.append(d)
         
         let e = MaterialModel(title: "Pose Bridge untuk Kesehatan Reproduksi", detail: "Pose jembatan? Yakin pose ini bisa membantu kesehatan reproduksi?", image: "materiBridge")
         arrMaterial3.append(e)
         
-//        let f = MaterialModel(title: "Olahraga intensitas tinggi tidak baik", detail: "Padahal ikan enak dan bermanfaat loh! Pelajari lebih lanjut!", image: "")
-//        arrMaterial.append(f)
+        
         
         var section1 = SectionMaterial(title: "Gaya Hidup", materials: arrMaterial)
         var section2 = SectionMaterial(title: "Kebersihan", materials: arrMaterial2)
